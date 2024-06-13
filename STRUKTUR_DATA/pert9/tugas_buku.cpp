50453d61b5c8a8232540ec03a3b06fdec1ae6cfc
h0murasak1 <farhanfatahillah10oib@gmail.com>
2024-06-13 18:46:25 +0700

    new

--- STRUKTUR_DATA/pert9/tugas_buku.cpp
+++ STRUKTUR_DATA/pert9/tugas_buku.cpp
@@ -7,10 +7,14 @@ using namespace std;
 int main()
 {
     stack<string> tumpukanBuku;
-    int operasi;
+    string operasi;
 
     while (true)
     {
+        fflush(stdin);
+        cout << "Tekan tombol apa saja untuk melanjutkan...";
+        cin.ignore();
+        system("clear");
         cout << "Masukkan operasi (push, pop, peek, isEmpty, size): \n";
         cout << "===================================================" << endl;
         cout << "1. push" << endl;
@@ -19,9 +23,10 @@ int main()
         cout << "4. isEmpty" << endl;
         cout << "5. size" << endl;
         cout << "6. exit" << endl;
+        cout << "Masukkan Pilihan : ";
         cin >> operasi;
 
-        if (operasi == 1)
+        if (operasi == "1")
         {
             string judul;
             cout << "Masukkan judul buku: ";
@@ -30,7 +35,7 @@ int main()
             tumpukanBuku.push(judul);
             cout << "Buku dengan judul \"" << judul << "\" berhasil ditambahkan ke dalam tumpukan." << endl;
         }
-        else if (operasi == 2)
+        else if (operasi == "2")
         {
             if (tumpukanBuku.empty())
             {
@@ -43,7 +48,7 @@ int main()
                 cout << "Buku dengan judul \"" << judul << "\" berhasil dihapus dari tumpukan." << endl;
             }
         }
-        else if (operasi == 3)
+        else if (operasi == "3")
         {
             if (tumpukanBuku.empty())
             {
@@ -55,7 +60,7 @@ int main()
                 cout << "Buku paling atas dalam tumpukan memiliki judul \"" << judul << "\"." << endl;
             }
         }
-        else if (operasi == 4)
+        else if (operasi == "4")
         {
             if (tumpukanBuku.empty())
             {
@@ -66,19 +71,21 @@ int main()
                 cout << "Tumpukan buku tidak kosong." << endl;
             }
         }
-        else if (operasi == 5)
+        else if (operasi == "5")
         {
             cout << "Jumlah buku dalam tumpukan: " << tumpukanBuku.size() << endl;
         }
-        else if (operasi == 6)
+        else if (operasi == "6")
         {
-            return false;
+            break;
         }
         else
         {
             cout << "Operasi tidak valid. Silakan coba lagi." << endl;
         }
     }
+
+    cout << "Terima kasih telah menggunakan program ini.";
 
     return 0;
 }
